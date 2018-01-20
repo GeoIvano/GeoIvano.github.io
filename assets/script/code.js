fbaf1abbd3b2254b4cc01237ed8ae908c85d81ed
GeoIvano <georgistoykovivanov@abv.bg>
2018-01-20 22:37:10 +0200

    Make responsive design

--- assets/script/code.js
+++ assets/script/code.js
@@ -1,6 +1,6 @@
 $(".button").on("click", function() {
     $(".wrapper").hide().addClass("wr").fadeIn(3000)
     $("#cup>img").hide().attr("src", "assets/img/cake.jpg").fadeIn(3000)
-    $(".button").hide().text('Честит рожден ден Меги. Мн здраве, успешно построени сгради и сбъднати мечти и цели.')
+    $(".button").hide().text('Честит рожден ден Меги. Мн здраве, щастие, успешно построени сгради и сбъднати мечти и цели.')
         .addClass("wish").fadeIn(3000)
 });
