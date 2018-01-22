ff1eeb9f50c9143098946b3f584c7dd93a694458
GeoIvano <georgistoykovivanov@abv.bg>
2018-01-22 15:32:11 +0200

    change timeing

--- assets/script/code.js
+++ assets/script/code.js
@@ -1,6 +1,10 @@
 $(".button").on("click", function() {
-    $(".wrapper").hide().addClass("wr").fadeIn(3000)
-    $("#cup>img").hide().attr("src", "assets/img/cake.jpg").fadeIn(3000)
-    $(".button").hide().addClass("wish").fadeIn(3000)
+    $(".wrapper").hide().addClass("wr").fadeIn(2500)
+    $("#cup>img").hide().attr("src", "assets/img/cake.jpg").fadeIn(2500)
+    $(".button")
+        .hide("fast")
+        .addClass("wish")
         .text('Честит рожден ден Меги. Мн здраве, щастие, успешно построени сгради и сбъднати мечти и цели.')
+        .fadeIn(2500)
+
 });
