043afca51779bc132036e6fa6a095382d376645f
GeoIvano <georgistoykovivanov@abv.bg>
2018-01-22 17:39:59 +0200

    final version

--- assets/script/code.js
+++ assets/script/code.js
@@ -2,6 +2,6 @@ $(".button").on("click", function() {
     $(".wrapper").hide().addClass("wr").fadeIn(2000)
     $("#cup>img").hide().attr("src", "assets/img/cake.jpg").fadeIn(2000)
     $(".button").hide("fast").addClass("wish")
-        .text('Честит рожден ден Меги. Мн здраве, щастие, успешно построени сгради и сбъднати мечти и цели.')
+        .text('Честит рожден ден Меги. Много здраве, щастие, успешно построени сгради и сбъднати мечти и цели !')
         .fadeIn(2000).off("click")
 });
