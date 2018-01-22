2a303594310102aac4ec68ed1e5fba9414f758b8
GeoIvano <georgistoykovivanov@abv.bg>
2018-01-22 15:47:05 +0200

    remove the click event

--- assets/script/code.js
+++ assets/script/code.js
@@ -1,10 +1,8 @@
 $(".button").on("click", function() {
     $(".wrapper").hide().addClass("wr").fadeIn(2500)
     $("#cup>img").hide().attr("src", "assets/img/cake.jpg").fadeIn(2500)
-    $(".button")
-        .hide("fast")
-        .addClass("wish")
+    $(".button").hide("fast").addClass("wish")
         .text('Честит рожден ден Меги. Мн здраве, щастие, успешно построени сгради и сбъднати мечти и цели.')
-        .fadeIn(2500)
+        .fadeIn(2500).off("click")
 
 });
