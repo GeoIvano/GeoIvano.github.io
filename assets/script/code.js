e4909dbbf03f384c59cd256cc45c5cd97dfac6d6
GeoIvano <georgistoykovivanov@abv.bg>
2018-02-23 07:40:51 +0200

    add document.ready function

--- assets/script/code.js
+++ assets/script/code.js
@@ -1,7 +1,9 @@
-$(".button").on("click", function() {
-    $(".wrapper").hide().addClass("wr").fadeIn(2000)
-    $("#cup>img").hide().attr("src", "assets/img/cake.jpg").fadeIn(2000)
-    $(".button").hide("fast").addClass("wish")
-        .text('Happy birthday !')
-        .fadeIn(2000).off("click")
-});
+$(document).ready(function() {
+    $(".button").on("click", function() {
+        $(".wrapper").hide().addClass("wr").fadeIn(2000)
+        $("#cup>img").hide().attr("src", "assets/img/cake.jpg").fadeIn(2000)
+        $(".button").hide("fast").addClass("wish")
+            .text('Happy birthday !')
+            .fadeIn(2000).off("click")
+    });
+})
